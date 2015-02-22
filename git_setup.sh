982bd417d435d8edf52b05ed3f55642d6d06edee
Stanley Xu <stanley.wux@gmail.com>
2015-02-22 14:00:48 -0800

    More system general git setup requiring email.

--- git_setup.sh
+++ git_setup.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+if [ -z "$1" ]; then
+  echo "Invalid command: too few arguments supplied."
+  echo "Usage: $ ./git_setup.sh \"user.email\""
+  exit 1
+fi
+
+
 # Global user settings }
-  git config --global user.email "stanley.wux@gmail.com"
+  git config --global user.email "$1"
   git config --global user.name "Stanley Xu"
 # }
 # Global ignore {
